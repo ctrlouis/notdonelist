eb6e35d2858c97dc85c9a5518488f42a26b9e74b
ctrlouis <louis.bty57@gmail.com>
2020-03-10 14:18:00 +0100

    fix btoa

--- app/components/Signin.vue
+++ app/components/Signin.vue
@@ -1,25 +1,67 @@
 <template>
     <StackLayout>
-        <TextField v-model="username" hint="Username"></TextField>
-        <TextField v-model="password" hint="Passsword"></TextField>
+        <TextField keyboardType="email" v-model="mail" hint="Email"></TextField>
+        <TextField secure="true" v-model="password" hint="Passsword"></TextField>
         <Button text="Signin" @tap="onSignin"></Button>
         <Label text="No account? Create one!" @tap="$emit('toggleAccount')"></Label>
     </StackLayout>
 </template>
 
 <script>
+import axios from 'axios';
+import btoa from 'btoa';
+
+if (!global.btoa) {
+    global.btoa = btoa;
+}
+
 export default {
   data: function() {
     return {
-      username: "",
+      mail: "",
       password: "",
     };
   },
 
   methods: {
-    onSignin: function() {
-        
-    }
+    checkForm: function() {
+          let error = "";
+          let errors = [];
+          
+          const emailRegex = RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+
+          if (this.mail == "") {
+              error = "Mail can't be empty.";
+              errors.push(error);
+          }
+
+          if (!emailRegex.test(this.mail)) {
+              error = "Use a valid adress mail";
+              errors.push(error);
+          }
+
+          return errors;
+      },
+      onSignin: function() {
+          const errors = this.checkForm();
+          if (errors.length > 0) {
+              alert(errors);
+          } else {
+                const basicAuth = {
+                    auth: {
+                        username: this.mail,
+                        password: this.password
+                    }
+                };
+
+                const url = "https://api.todolist.sherpa.one/users/signin";
+                axios.post(url, {}, basicAuth)
+                    .then((result) => {
+                        alert(result.data.token);
+                    })
+                    .catch(err => alert(err.message));
+          }
+      }
   }
 };
 </script>
