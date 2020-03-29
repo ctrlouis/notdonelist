85ef135cff2c91b720b97836bb8042d2986f6161
ctrlouis <louis.bty57@gmail.com>
2020-03-29 01:19:16 +0100

    update indentation

--- app/components/TodoCreate.vue
+++ app/components/TodoCreate.vue
@@ -1,30 +1,30 @@
 <template>
-  <Page>
-    <StackLayout>
-      <Label text="Add new item"></Label>
-      <TextField v-model="newTask" hint="I have to..."></TextField>
-      <Button text="Save" @tap="onSaveTap"></Button>
-    </StackLayout>
-  </Page>
+	<Page>
+		<StackLayout>
+			<Label text="Add new item"></Label>
+			<TextField v-model="task" hint="I have to..."></TextField>
+			<Button text="Save" @tap="onSaveTap"></Button>
+		</StackLayout>
+	</Page>
 </template>
 
 <script>
 export default {
-  data: function() {
-    return {
-      newTask: ""
-    };
-  },
+	data: function() {
+		return {
+			task: ""
+		};
+	},
 
-  methods: {
-    onSaveTap: function() {
-      const newTask = {
-        message: this.newTask,
-        done: false,
-        deleted: false
-      };
-      this.$modal.close(newTask);
-    }
-  }
+	methods: {
+			onSaveTap() {
+				const newTask = {
+					message: this.task,
+					done: false,
+					deleted: false
+				};
+				this.$modal.close(newTask);
+			}
+	}
 };
 </script>
