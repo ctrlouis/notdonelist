4ff036d9c1510aa1a29919eeb9245a85459c181c
ctrlouis <louis.bty57@gmail.com>
2020-03-30 15:02:31 +0200

    add camera

--- app/components/TodoCreate.vue
+++ app/components/TodoCreate.vue
@@ -3,6 +3,11 @@
 		<StackLayout>
 			<Label text="Add new item"></Label>
 			<TextField v-model="task" hint="I have to..."></TextField>
+			<Button text="Take Picture" @tap="takePicture" />
+			<Button text="Choose Picture" @tap="selectPicture" />
+			<WrapLayout>
+				<Image v-for="img in images" :src="img.src" width="75" height="75" />
+			</WrapLayout>
 			<Button text="Save" @tap="onSaveTap"></Button>
 			<Button text="Cancel" @tap="onCancelTap"></Button>
 		</StackLayout>
@@ -14,6 +19,9 @@ import axios from 'axios';
 import btoa from 'btoa';
 import { Couchbase, ConcurrencyMode } from 'nativescript-couchbase-plugin';
 import dialogs from 'tns-core-modules/ui/dialogs';
+import * as camera from "nativescript-camera";
+import * as imagepicker from "nativescript-imagepicker";
+import { Image } from "tns-core-modules/ui/image";
 import conf from './../js/conf.json';
 
 if (!global.btoa) global.btoa = btoa;
@@ -27,6 +35,7 @@ export default {
 		return {
 			task: "",
 			credentials: null,
+			images: []
 		};
 	},
 
@@ -77,6 +86,49 @@ export default {
 					reject(err);
 				});
             });
+		},
+
+		selectPicture() {
+
+			let context = imagepicker.create({
+				mode: 'multiple' 
+			});
+
+			context.authorize()
+			.then(function() {
+				return context.present();
+			})
+			.then(selection => {
+				selection.forEach(selected => {
+					
+					console.log(JSON.stringify(selected));
+
+					let img = new Image();
+					img.src = selected;
+					this.images.push(img);
+				});
+			}).catch(function (e) {
+				console.log('error in selectPicture', e);
+			});
+
+		},
+		takePicture() {
+			camera.requestPermissions()
+			.then(() => {
+				camera.takePicture({ width: 300, height: 300, keepAspectRatio: true, saveToGallery:false })
+				.then(imageAsset => {
+					let img = new Image();
+					img.src = imageAsset;
+					this.images.push(img);
+					console.log('ive got '+this.images.length+' images now.');
+				})
+				.catch(e => {
+					console.log('error:', e);
+				});
+			})
+			.catch(e => {
+				console.log('Error requesting permission');
+			});
 		}
 	},
 
