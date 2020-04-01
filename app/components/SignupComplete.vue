9d2a01045483fbf891882e39dd8944562043ea04
ctrlouis <louis.bty57@gmail.com>
2020-04-01 21:46:05 +0200

    add better credentials display

--- app/components/SignupComplete.vue
+++ app/components/SignupComplete.vue
@@ -1,26 +1,11 @@
 <template>
     <Page>
-        <GridLayout columns="*, 2*" rows="2*, 3*">
-            <Label text="Your credentials" row="0" col="0" class="center" />
-            <TextView editable="false" row="1" col="0" class="left">
-                <FormattedString>
-                    <Span text="Email :" fontWeight="Bold" />
-                    <Span text="Password : " fontWeight="Bold" />
-                </FormattedString>
-            </TextView>
-            <TextView editable="false" row="1" col="1" class="right">
-                <FormattedString>
-                    <Span text="You can use text attributes such as " />
-                    <Span text="bold, " fontWeight="Bold" />
-                    <Label :text="data.email" />
-                    <Label :text="data.password" />
-                </FormattedString>
-            </TextView>
-        </GridLayout>
         <StackLayout>
-            <Label text="Keep this information !"/>
-            <Label :text="data.email"/>
-            <Label :text="data.password"/>
+            <Label text="Save your credentials" class="center" />
+            <Span text="Email :" fontWeight="Bold" />
+            <Span text="Password : " fontWeight="Bold" />
+            <Label :text="data.email" />
+            <Label :text="data.password" />
             <Button text="Close" @tap="onConfirm"></Button>
         </StackLayout>
     </Page>
