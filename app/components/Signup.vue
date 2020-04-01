ff92e7a825a1377811545193944dac54748e2e20
ctrlouis <louis.bty57@gmail.com>
2020-04-01 21:44:34 +0200

    fix signup

--- app/components/Signup.vue
+++ app/components/Signup.vue
@@ -3,8 +3,10 @@
         <TextField keyboardType="email" v-model="mail" hint="Mail"></TextField>
         <TextField v-model="firstname" hint="FirstName"></TextField>
         <TextField v-model="lastname" hint="Lastname"></TextField>
-        <Label :text="gender.select"></Label>
-        <Switch v-model="gender.switch" @checkedChange="toggleGender"></Switch>
+        <FlexboxLayout justifyContent="center">
+            <Button text="Male" class="gender" :class="maleSelected" @tap="selectMale"></Button>
+            <Button text="Female" class="gender" :class="femaleSelected" @tap="selectFemale"></Button>
+        </FlexboxLayout>
         <Button text="Signup" @tap="onSignup"></Button>
         <Label text="Already have account? Signin here" class="link" @tap="$emit('toggleAccount')"></Label>
     </StackLayout>
@@ -16,94 +18,137 @@ import axios from 'axios';
 
 
 export default {
-  data: function() {
-    return {
-      mail: "",
-      firstname: "",
-      lastname: "",
-      gender: {
-          switch: true,
-          select: "male"
-      }
-    };
-  },
+    data: function() {
+        return {
+            mail: "",
+            firstname: "",
+            lastname: "",
+            gender: null
+        };
+    },
 
-  methods: {
-      toggleGender: function() {
-          if (this.gender == "male") {
-              this.gender = "female"
-          } else {
-              this.gender = "male"
-          }
-      },
-      checkForm: function() {
-          let error = "";
-          let errors = [];
-          
-          const emailRegex = RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-          const nameRegex = RegExp("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+    methods: {
+        onSignup: function() {
+            const errors = this.checkForm();
+            if (errors.length > 0) {
+                alert(errors);
+            } else {
+                    const newUser = {
+                        firstname: this.firstname,
+                        lastname: this.lastname,
+                        email: this.mail,
+                        gender: this.gender
+                    };
 
-          if (this.mail == "") {
-              error = "Mail can't be empty.";
-              errors.push(error);
-          }
+                    const url = "https://api.todolist.sherpa.one/users/signup";
+                    axios.post(url, newUser)
+                        .then((result) => {
+                            const options = {
+                                props: {
+                                    data: result.data
+                                }
+                            };
+                            this.$showModal(SignupComplete, options).then(this.$emit('toggleAccount'));
+                        })
+                        .catch(err => {
+                            const errorDetails = err.response.request._response;
+                            console.log(errorDetails);
+                            alert({
+                                title: "Error",
+                                message: "Mail already used.",
+                                okButtonText: "Ok"
+                            });
+                        });
+            }
+        },
 
-          if (this.firstname == "") {
-              error = "Firstname can't be empty.";
-              errors.push(error);
-          }
+        selectMale() {
+            this.gender = 'male';
+        },
 
-          if (this.lastname == "") {
-              error = "Last can't be empty.";
-              errors.push(error);
-          }
+        selectFemale() {
+            this.gender = 'female';
+        },
+        
+        checkForm: function() {
+            let error = "";
+            let errors = [];
+            
+            const emailRegex = RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+            const nameRegex = RegExp("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
 
-          if (!emailRegex.test(this.mail)) {
-              error = "Use a valid adress mail";
-              errors.push(error);
-          }
+            if (this.mail == "") {
+                error = "Mail can't be empty.";
+                errors.push(error);
+            }
 
-          if (!nameRegex.test(this.firstname)) {
-              error = "Use a valid firstname";
-              errors.push(error);
-          }
+            if (this.firstname == "") {
+                error = "Firstname can't be empty.";
+                errors.push(error);
+            }
 
-          if (!nameRegex.test(this.lastname)) {
-              error = "Use a valid lastname";
-              errors.push(error);
-          }
+            if (this.lastname == "") {
+                error = "Last can't be empty.";
+                errors.push(error);
+            }
 
-          return errors;
-      },
-      onSignup: function() {
-          const errors = this.checkForm();
-          if (errors.length > 0) {
-              alert(errors);
-          } else {
-                const newUser = {
-                    firstname: this.firstname,
-                    lastname: this.lastname,
-                    email: this.mail,
-                    gender: this.gender
-                };
+            if (!emailRegex.test(this.mail)) {
+                error = "Use a valid adress mail";
+                errors.push(error);
+            }
 
-                const url = "https://api.todolist.sherpa.one/users/signup";
-                axios.post(url, newUser)
-                    .then((result) => {
-                        const options = {
-                            props: {
-                                data: result.data
-                            }
-                        };
-                        this.$showModal(SignupComplete, options).then(this.$emit('toggleAccount'));
-                    })
-                    .catch(err => alert(err.message));
-          }
-      }
-  }
+            if (!nameRegex.test(this.firstname)) {
+                error = "Use a valid firstname";
+                errors.push(error);
+            }
+
+            if (!nameRegex.test(this.lastname)) {
+                error = "Use a valid lastname";
+                errors.push(error);
+            }
+
+            if (!this.gender) {
+                error = "Select a gender";
+                errors.push(error);
+            }
+
+            return errors;
+        }
+    },
+
+    computed: {
+        maleSelected() {
+            if (this.gender == 'male') {
+                return 'male';
+            } else {
+                return 'gender';
+            }
+        },
+
+        femaleSelected() {
+            if (this.gender == 'female') {
+                return 'female';
+            } else {
+                return 'gender';
+            }
+        }
+    }
 };
 </script>
 
 <style scoped>
+.gender {
+    background-color: darkgray;
+    color: black;
+}
 
+.male {
+    background-color: magenta;
+    color: white;
+}
+
+.female {
+    background-color: aqua;
+    color: white;
+}
 </style>
